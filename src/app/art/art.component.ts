0a23c03c44bf5be5fd2759d01041fdeb084268d2
Serena <serena_chan@outlook.com>
2020-12-28 00:23:38 +0000

    Updated art section

--- src/app/art/art.component.ts
+++ src/app/art/art.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-art',
   templateUrl: './art.component.html',
   styleUrls: ['./art.component.scss']
 })
-export class ArtComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ArtComponent {
+  landscapes = ["2012_dec.JPG", "2012_nov.JPG", "iceland1.jpg", "iceland2.jpg", "iceland3.JPG", "iceland4.JPG"]
+  portraitLandscapes = ["venice_cube.jpg", "morroco.jpg"]
+  portraits = ["study.jpeg","melancholy.jpg","still.jpg", "oilpainting1.jpg"]
+  abstracts = ["insomnia.jpg", "flow.jpeg"]
 }
