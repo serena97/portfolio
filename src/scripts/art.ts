79a8ef94857bff8e94d97e0149adf7c4838ae667
Serena <serena.chan@ft.com>
2022-06-13 21:25:13 +0100

    added photoswipe gallery and art html scss

--- src/scripts/art.ts
+++ src/scripts/art.ts
@@ -1,7 +1,19 @@
 import { initMenuListener } from './common.js'
+import PhotoSwipeLightbox from '../../photoswipe/dist/photoswipe-lightbox.esm.js';
+import PhotoSwipe from '../../photoswipe/dist/photoswipe.esm.js';
+
+function initGallery() {
+    const lightbox = new PhotoSwipeLightbox({
+        gallery: '#my-gallery',
+        children: 'a',
+        pswpModule: PhotoSwipe
+    });
+    lightbox.init();
+}
 
 function main() {
     initMenuListener();
+    initGallery();
 }
 
 main();
